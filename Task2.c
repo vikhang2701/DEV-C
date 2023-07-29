de8ff258ec2d98cc8f30d3b42e1c4efd7c44cd7e
vikhang01 <pkhang0111@gmail.com>
2023-07-29 20:12:24 +0700

    Lesson 5.1

--- Task2.c
+++ Task2.c
@@ -23,3 +23,16 @@ int main()
 
     
 }*/
+#include<stdio.h>
+int main()
+{ 
+    int a = 5;
+    int b = 5;
+    printf("%d",a++);
+    printf("\n%d",a);
+    printf("\n%d",++a);
+
+    printf("\n%d",b--);
+    printf("\n%d",b);
+    printf("\n%d",--b);
+}

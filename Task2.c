afb3b9378a9768ecfff83f887148f420618d590f
vikhang01 <pkhang0111@gmail.com>
2023-07-29 19:59:24 +0700

    [Lesson3]

--- Task2.c
+++ Task2.c
@@ -2,10 +2,24 @@
 
 int main()
 {
-    int a;
-    int b;
-    int c = a + b;
-    printf("nhap 2 so a b : \n");
-    scanf("%d %d",&a,&b);
-    printf("so c la %d :\n",c);
+    float a,b;
+    printf("Nhap vao 2 so a b : ");
+    scanf("%f %f",&a,&b);
+    float tong = a + b;
+    float hieu = a - b;
+    float tich = a * b;
+    float thuong = a/b;
+    int c = ((int) a) % ((int) b);
+
+    printf("gia tri tong la %.2f + %.2f = %.2f",a,b,tong);
+    printf("\ngia tri hieu la %.2f - %.2f = %.2f",a,b,hieu);
+    printf("\ngia tri tich la %.2f * %.2f = %.2f",a,b,tich);
+    printf("\ngia tri thuong la %.2f / %.2f = %.2f",a,b,thuong);
+    printf("\ngia tri c la %.2f / %.2f = %d",a,b,c);
+    a++;
+    b--;
+    printf("\nso a hien tai %f ",a);
+    printf("\nso b hien tai %f",b);
+
+    
 }
